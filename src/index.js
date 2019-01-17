f685d9c3c45f0bea4f7b7a0f6754d2151b420db1
Mathew Cormier <mathew.corm@gmail.com>
2019-01-17 11:40:06 -0500

    Add contracts to npm files

--- src/index.js
+++ src/index.js
@@ -25,7 +25,6 @@ export class CommentThread extends React.Component {
     }
 
     async init() {
-      await wait(500)
 
       if (this.props.aragonApp) {
         this.hasCommentsAppAddress = await this.getContractAddress()
@@ -153,7 +152,6 @@ const Main = styled.div`
     border: 1px solid #ddd;
     border-radius: 4px;
     padding: 10px;  
-    margin-left: 10px;  
 `
 
 const InputContainer = styled.div`
